f05f682733f9af5a7c9a9d56113ae4c64af4e7a2
Andrei Ignat <ignatandrei@yahoo.com>
2022-08-27 16:02:34 +0300

    feat: more operators

--- src/RxJSDemo/RxjsAng/src/app/classes/ObsDataSerializable.ts
+++ src/RxJSDemo/RxjsAng/src/app/classes/ObsDataSerializable.ts
@@ -1,10 +1,15 @@
+import { Observable } from "rxjs";
 import { ObservableData } from "./ObservableData";
 
-export class ObsDataSerializable{
+export class ObsDataSerializable {
 
-  public Data= new Map<string, ObservableData>();
+  public Data = new Map<string, ObservableData>();
   constructor() {
     this.Data.set("simpleMap", ObservableData.fromJSON('{"source":1,"numberOperators":1,"whatOperator":[{"operatorToApply":"map","valueToApply":"10","functionToApply":"multiply"}],"startNumbers":{"fromNumber":2,"count":7,"repeat":1,"delaySec":2}}'));
     this.Data.set("startDistinct", ObservableData.fromJSON('{"source":1,"numberOperators":2,"whatOperator":[{"operatorToApply":"startWith","valueToApply":"4","functionToApply":"numberToTake"},{"operatorToApply":"distinct","valueToApply":"10"}],"startNumbers":{"fromNumber":2,"count":7,"repeat":1,"delaySec":2}}'))
-  }  
+    this.Data.set("skipLast", ObservableData.fromJSON('{"source":1,"numberOperators":2,"whatOperator":[{"operatorToApply":"startWith","valueToApply":"4","functionToApply":"numberToTake"},{"operatorToApply":"distinct","valueToApply":"10"}],"startNumbers":{"fromNumber":2,"count":7,"repeat":1,"delaySec":2}}'));
+    this.Data.set("takeLast", ObservableData.fromJSON('{"source":1,"numberOperators":2,"whatOperator":[{"operatorToApply":"startWith","valueToApply":"4","functionToApply":"numberToTake"},{"operatorToApply":"takeLast","valueToApply":"2","functionToApply":"numberToTake"}],"startNumbers":{"fromNumber":2,"count":7,"repeat":1,"delaySec":2}}'));
+    this.Data.set("delay", ObservableData.fromJSON('{"source":1,"numberOperators":2,"whatOperator":[{"operatorToApply":"startWith","valueToApply":"4","functionToApply":"numberToTake"},{"operatorToApply":"delay","valueToApply":"10000","functionToApply":"numberToTake"}],"startNumbers":{"fromNumber":2,"count":7,"repeat":1,"delaySec":2}}'))
+    this.Data.set("debounceTime", ObservableData.fromJSON('{"source":1,"numberOperators":2,"whatOperator":[{"operatorToApply":"startWith","valueToApply":"4","functionToApply":"numberToTake"},{"operatorToApply":"debounceTime","valueToApply":"10000","functionToApply":"numberToTake"}],"startNumbers":{"fromNumber":2,"count":7,"repeat":1,"delaySec":2}}'));
+  }
 }
